ac32196e1304870c8fa9ed280cbcb266578debe5
Eduardo Zimmermann <z.edufz@gmail.com>
2024-04-09 16:44:50 -0300

    fix: Refatorado testes na classe ContaControllerTest

--- src/test/java/br/com/finance/DigitalBank/controller/ContaControllerTest.java
+++ src/test/java/br/com/finance/DigitalBank/controller/ContaControllerTest.java
@@ -1,22 +1,34 @@
 package br.com.finance.DigitalBank.controller;
 
+import br.com.finance.DigitalBank.api.TaxaRendApi;
+import br.com.finance.DigitalBank.entity.Conta;
+import br.com.finance.DigitalBank.entity.ContaCorrente;
+import br.com.finance.DigitalBank.entity.ContaPoupanca;
 import br.com.finance.DigitalBank.service.ContaService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@AutoConfigureJsonTesters
 class ContaControllerTest {
 
     @Autowired
@@ -25,14 +37,20 @@ class ContaControllerTest {
     @MockBean // Indica ao Spring para não usar o próprio ContaService, mas sim simulá-lo
     private ContaService contaService;
 
+    @Autowired
+    private JacksonTester<ContaCorrente> jsonContaCorrente; // Classe que gera o JSON
+
+    @Autowired
+    private JacksonTester<ContaPoupanca> jsonContaPoupanca;
+
     @Test
     void shouldReturn201WithSaveContaCorrente() throws Exception {
 
-        String json = "{}";
+        ContaCorrente contaCorrente = new ContaCorrente(new BigDecimal(12), LocalDate.of(2024, 07, 15));
 
-        var response = mockMvc.perform(
+        MockHttpServletResponse response = mockMvc.perform(
                 post("/conta/saveContaCorrente")
-                        .content(json)
+                        .content(jsonContaCorrente.write(contaCorrente).getJson())
                         .contentType(MediaType.APPLICATION_JSON)
         ).andReturn().getResponse();
 
@@ -43,11 +61,11 @@ class ContaControllerTest {
     @Test
     void shouldReturn201WithSaveContaPoupanca() throws Exception {
 
-        String json = "{}";
+        ContaPoupanca contaPoupanca = new ContaPoupanca(TaxaRendApi.getTaxaCdi(), LocalDate.of(2024, 06, 10));
 
-        var response = mockMvc.perform(
+        MockHttpServletResponse response = mockMvc.perform(
                 post("/conta/saveContaPoupanca")
-                        .content(json)
+                        .content(jsonContaPoupanca.write(contaPoupanca).getJson())
                         .contentType(MediaType.APPLICATION_JSON)
         ).andReturn().getResponse();
 
